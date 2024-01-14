a7c86aee8a9ffc6832c93910bf3fa6d12a50e0a0
Antaressa <katrin.tsalko@gmail.com>
2024-01-14 16:26:10 +0100

    changing selectionSorting function

--- Selection_sorting/Selection_sorting/Selection_sorting.cpp
+++ Selection_sorting/Selection_sorting/Selection_sorting.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 int findSmallest(int* mass, size_t size) {
 
@@ -6,7 +7,7 @@ int findSmallest(int* mass, size_t size) {
     int smallest_element_index = 0;
 
     for (int i = 1; i < size; i++) {
-        if (mass[i] < smallest) {
+        if (mass[i] < smallest && mass[i] != NULL) {
             smallest = mass[i];
             smallest_element_index = i;
         }
@@ -15,20 +16,34 @@ int findSmallest(int* mass, size_t size) {
     return smallest_element_index;
 }
 
-void selectionSorting(int *new_mass, int* mass, size_t size) {
+
+void selectionSorting(int* new_mass, int* mass, size_t size) {
 
     int i = 0;
 
-    while (i != size - 1) {
-        new_mass[i] = mass[findSmallest(mass, size)];
+    while (i != size) {
+        int pos = findSmallest(mass, size);
+        new_mass[i] = mass[pos];
+        mass[pos] = INT_MAX;
+        i++;
     }
+
+    delete[] mass;
 }
+
 
 int main()
 {
-    int* mass = new int[6] { 3, 8, 4, 1, 85, 24 };
-    int* new_mass = new int[6];
     size_t size = 6;
+    int* mass = new int[size] { 3, 8, 4, 1, 85, 24 };
+    int* new_mass = new int[size];
 
-    std::cout << findSmallest(mass, size) << std::endl;
+    selectionSorting(new_mass, mass, size);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << new_mass[i] << " ";
+    }
+
+    cout << endl;
 }
